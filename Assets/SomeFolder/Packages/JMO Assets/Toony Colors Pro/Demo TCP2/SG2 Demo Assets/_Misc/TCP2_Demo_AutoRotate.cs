67676d207bbe01194237988ac25d37a462582ae5
XsavaronaX <ikra@xenolia.com>
2023-06-09 16:15:38 +0300

    post procces

---
+++ Assets/SomeFolder/Packages/JMO Assets/Toony Colors Pro/Demo TCP2/SG2 Demo Assets/_Misc/TCP2_Demo_AutoRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TCP2_Demo_AutoRotate : MonoBehaviour
+{
+	public Vector3 axis = Vector3.up;
+	public float Speed = -50f;
+
+	void Update()
+	{
+		this.transform.Rotate(axis, Time.deltaTime * Speed);
+	}
+}
